0d3bd7eec955c4f0329442bb40d417c7a0b3bb30
Leo Girard <leo.girard@epitech.eu>
2026-04-21 12:37:48 +0200

    feat : import false token from .env into main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,22 @@
 import express, { response } from "express";
 import type { Request, Response } from 'express';
+import { env } from "node:process";
+import 'dotenv/config'
 
+const Token = process.env.TOKEN
+console.log(Token)
 
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req : Request, res : Response) => {
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
