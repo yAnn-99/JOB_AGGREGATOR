3a04a234aa96113081a865aa969abe56f906a986
ZacharyDevProjects <z.joriot@gmail.com>
2026-04-30 09:34:09 +0200

    chore: add base for front-end

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -1,27 +1,29 @@
 const Layout = ({ children }: {children: React.ReactNode}) => {
     return (
-        <header>
-            <div className="flex items-center justify-around p-8">
-                <h1 className="font-semibold text-lg">Jobly</h1>
-                <div className="flex items-center gap-32">
-                    <nav>
-                        <ul className="flex items-center gap-16">
-                            <li>jobs</li>
-                            <li>entreprise</li>
-                            <li>salaire</li>
-                            <li>conseils</li>
-                        </ul>
-                    </nav>
-                    <nav>
-                        <ul className="flex items-center gap-10">
-                            <li>sign up</li>
-                            <li className="px-4 py-2 rounded-md bg-indigo-500 text-white hover:bg-indigo-600 transition">inscription</li>
-                        </ul>
-                    </nav>
+        <div>
+            <header>
+                <div className="flex items-center justify-around p-8">
+                    <h1>Jobly</h1>
+                    <div className="flex items-center gap-32">
+                        <nav>
+                            <ul className="flex items-center gap-16">
+                                <li>jobs</li>
+                                <li>entreprise</li>
+                                <li>salaire</li>
+                                <li>conseils</li>
+                            </ul>
+                        </nav>
+                        <nav>
+                            <ul className="flex items-center gap-10">
+                                <li>sign up</li>
+                                <li className="px-4 py-2 rounded-md bg-indigo-500 text-white hover:bg-indigo-600 transition">inscription</li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-            </div>
+            </header>
             {children}
-        </header>
+        </div>
     )
 }
 
--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,10 +1,27 @@
 const Page = () => {
-    return (
-        <div>
-            <main>hero section</main>
-            <footer></footer>
+  return (
+    <main>
+      <div className="flex items-center p-8">
+        <div className="flex flex-col items-start gap-4">
+          <h1 className="text-6xl">
+            Trouvez le Job <br /> qui VOUS correspond
+          </h1>
+          <h2 className="text-l">
+            des millies d'offres d'emploies pour booster votre carriere
+            professionnel
+          </h2>
+          <div className="items-center overflow-hidden rounded-md border">
+            <input
+              type="text"
+              className="px-3 py-2 outline-none"
+              placeholder="Rechercher..."
+            />
+            <button className="bg-blue-500 px-4 py-2 text-white">search</button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </main>
+  );
+};
 
 export default Page;
