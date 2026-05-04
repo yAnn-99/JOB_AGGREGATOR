060bdd274c859456bfbefa0a9f3a649919c1c949
ZacharyDevProjects <z.joriot@gmail.com>
2026-05-04 10:49:27 +0200

    chore: sign up and log in page

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -8,12 +8,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <div>
       <header className="border-b">
         <div className="flex items-center justify-between p-4 md:p-8">
-          <h1 className="text-xl md:text-2xl font-bold"><a href="/">Jobly</a></h1>
+          <h1 className="text-xl md:text-2xl font-bold">
+            <a href="/">Jobly</a>
+          </h1>
 
           <div className="hidden md:flex items-center gap-12">
             <nav>
               <ul className="flex items-center gap-8">
-                <li><a href="/compagnies">jobs</a></li>
+                <li>
+                  <a href="/compagnies">jobs</a>
+                </li>
                 <li>entreprise</li>
                 <li>salaire</li>
                 <li>conseils</li>
@@ -21,10 +25,13 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             </nav>
 
             <nav>
+              <a href=""></a>
               <ul className="flex items-center gap-6">
-                <li>sign up</li>
+                <li>
+                  <a href="/log_in">log in</a>
+                </li>
                 <li className="px-4 py-2 rounded-md bg-indigo-500 text-white hover:bg-indigo-600 transition">
-                  inscription
+                  <a href="/sign_up">sign up</a>
                 </li>
               </ul>
             </nav>
@@ -37,18 +44,24 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
         {open && (
           <div className="md:hidden px-4 pb-4">
-            <ul className="flex flex-col gap-4">
-              <li>jobs</li>
-              <li>entreprise</li>
-              <li>salaire</li>
-              <li>conseils</li>
-            </ul>
+            <nav>
+              <ul className="flex flex-col gap-4">
+                <li>jobs</li>
+                <li>entreprise</li>
+                <li>salaire</li>
+                <li>conseils</li>
+              </ul>
+            </nav>
 
             <div className="mt-4 flex flex-col gap-3">
-              <button>sign up</button>
-              <button className="px-4 py-2 bg-indigo-500 text-white rounded-md">
-                inscription
-              </button>
+              <a href="/log_in">
+                <button>log in</button>
+              </a>
+              <a href="/sign_up">
+                <button className="px-4 py-2 bg-indigo-500 text-white rounded-md">
+                  sign up
+                </button>
+              </a>
             </div>
           </div>
         )}
