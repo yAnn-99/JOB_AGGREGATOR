78fc6cd11c7723b07d907120a7c7ed21d614b47a
Leo Girard <leo.girard@epitech.eu>
2026-05-05 11:20:44 +0200

    feat : connection between front and back done, removal https

--- frontend/app/(log in)/sign_up/page.tsx
+++ frontend/app/(log in)/sign_up/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Link from "next/link";
 
 const sign_up = () => {
-  const formData = useState({
+  const [formData, setFormData] = useState({
     firstname: "",
     lastname: "",
     email: "",
@@ -11,23 +11,32 @@ const sign_up = () => {
     passwordConf: "",
   });
 
-  if (formData.password != formData.passwordConf) {
-    alert('Wrong password')
-    return;
-  }
 
-  const handleSubmit = async (e) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const OnSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (formData.password != formData.passwordConf) {
+      alert('Wrong password')
+      return;
+    }
 
 
     try {
-      const response = await fetch('https://localhost:3000/register', {
+      const response = await fetch('http://localhost:3000/register', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
+        credentials : 'include',
         body: JSON.stringify({
-          firstname : formData.firstname,
-          lastname : formData.lastname,
-          email : formData.email,
+          firstname: formData.firstname,
+          lastname: formData.lastname,
+          email: formData.email,
           password: formData.password
         }),
 
@@ -40,202 +49,214 @@ const sign_up = () => {
         alert(result.message)
       }
     } catch (error) {
-      console.log(error)
+      alert(error)
     }
+  }
 
-    return (
-      <div>
-        <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Create your account
-            </h2>
-          </div>
+  return (
+    <div>
+      <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Create your account
+          </h2>
+        </div>
 
-          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-              <form className="space-y-6" action="#" method="POST">
-                <div>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" onSubmit={OnSubmit}>
+              <div>
+                <label
+                  htmlFor="Firstname"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Firstname
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="Firstname"
+                    name="firstname"
+                    type="text"
+                    required
+                    value={formData.firstname}
+                    onChange={onChange}
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your Firstname"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="Lastname"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Lastname
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="Lastname"
+                    name="lastname"
+                    type="text"
+                    required
+                    value={formData.lastname}
+                    onChange={onChange}
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your Lastname"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    value={formData.email}
+                    onChange={onChange}
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your email address"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    value={formData.password}
+                    onChange={onChange}
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your password"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  confirm password
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="passwordConfirmation"
+                    name="passwordConf"
+                    type="password"
+                    required
+                    value={formData.passwordConf}
+                    onChange={onChange}
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your password"
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember_me"
+                    name="remember_me"
+                    type="checkbox"
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+
                   <label
-                    htmlFor="Firstname"
-                    className="block text-sm font-medium text-gray-700"
+                    htmlFor="remember_me"
+                    className="ml-2 block text-sm text-gray-900"
                   >
-                    Firstname
+                    Remember me
                   </label>
-                  <div className="mt-1">
-                    <input
-                      id="Firstname"
-                      name="Firstname"
-                      type="text"
-                      required
-                      className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                      placeholder="Enter your Firstname"
-                    />
-                  </div>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+            <div className="mt-6">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300"></div>
                 </div>
 
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 bg-gray-100 text-gray-500">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-6 grid grid-cols-3 gap-3">
                 <div>
-                  <label
-                    htmlFor="Lastname"
-                    className="block text-sm font-medium text-gray-700"
+                  <Link
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                   >
-                    Lastname
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="Lastname"
-                      name="Lastname"
-                      type="text"
-                      required
-                      className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                      placeholder="Enter your Lastname"
+                    <img
+                      className="h-5 w-5"
+                      src="https://www.svgrepo.com/show/512120/facebook-176.svg"
+                      alt=""
                     />
-                  </div>
+                  </Link>
                 </div>
-
                 <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium text-gray-700"
+                  <Link
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                   >
-                    Email address
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                      placeholder="Enter your email address"
+                    <img
+                      className="h-5 w-5"
+                      src="https://www.svgrepo.com/show/513008/twitter-154.svg"
+                      alt=""
                     />
-                  </div>
+                  </Link>
                 </div>
-
                 <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
+                  <Link
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                   >
-                    Password
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                      placeholder="Enter your password"
+                    <img
+                      className="h-6 w-6"
+                      src="https://www.svgrepo.com/show/506498/google.svg"
+                      alt=""
                     />
-                  </div>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    confirm password
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="passwordConfirmation"
-                      name="passwordConfirmation"
-                      type="password"
-                      required
-                      className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                      placeholder="Enter your password"
-                    />
-                  </div>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center">
-                    <input
-                      id="remember_me"
-                      name="remember_me"
-                      type="checkbox"
-                      className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                    />
-
-                    <label
-                      htmlFor="remember_me"
-                      className="ml-2 block text-sm text-gray-900"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
-
-                <div>
-                  <button
-                    type="submit"
-                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Sign in
-                  </button>
-                </div>
-              </form>
-              <div className="mt-6">
-                <div className="relative">
-                  <div className="absolute inset-0 flex items-center">
-                    <div className="w-full border-t border-gray-300"></div>
-                  </div>
-
-                  <div className="relative flex justify-center text-sm">
-                    <span className="px-2 bg-gray-100 text-gray-500">
-                      Or continue with
-                    </span>
-                  </div>
-                </div>
-
-                <div className="mt-6 grid grid-cols-3 gap-3">
-                  <div>
-                    <Link
-                      href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
-                    >
-                      <img
-                        className="h-5 w-5"
-                        src="https://www.svgrepo.com/show/512120/facebook-176.svg"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div>
-                    <Link
-                      href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
-                    >
-                      <img
-                        className="h-5 w-5"
-                        src="https://www.svgrepo.com/show/513008/twitter-154.svg"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div>
-                    <Link
-                      href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
-                    >
-                      <img
-                        className="h-6 w-6"
-                        src="https://www.svgrepo.com/show/506498/google.svg"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default sign_up;
+
+export default sign_up;
