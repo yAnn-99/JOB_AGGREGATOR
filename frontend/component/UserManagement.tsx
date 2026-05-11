23fc4d18bcc1fabd0739835e61346baac5b92657
Leo Girard <leo.girard@epitech.eu>
2026-05-11 16:51:59 +0200

    feat : styling the admin page

--- frontend/component/UserManagement.tsx
+++ frontend/component/UserManagement.tsx
@@ -1,5 +1,10 @@
 'use client';
-
+import Button from '@mui/material/Button';
+import {red}from   '@mui/material/colors';
+import { TextField } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CancelIcon from '@mui/icons-material/Cancel';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { useState } from "react";
 
 
@@ -59,15 +64,19 @@ export default function ManageUser({ userId }: { userId?: string }) {
     return (
 
 
-        <div className="flex mt-5 flex-col lg:flex-row-reverse  gap-4 p-4 w-full">
-            
-            <button onClick={UnblockUser} className="lg:mr-6 lg:pr-3 lg:pl-3 bg-green-500 hover:bg-green-700 p-2 rounded-xl ">Unblock</button>
-            <button onClick={blockUser} className="lg:mr-6 lg:pr-3 lg:pl-3 bg-amber-500 hover:bg-amber-700 p-2 rounded-xl">Block</button>
-            <button onClick={delUser} className="lg:mr-6 lg:pr-3 lg:pl-3 bg-red-800 hover:bg-red-950 p-2 rounded-xl">Delete</button>
-            <form className="mr-6 " onSubmit={(e) => e.preventDefault()}>
-                <label className="font-extrabold"> Id: </label>
-                <input type="text" name="id" value={TrueId} onChange={(e) => SetId(e.target.value)} className="border-2 w-full" />
-            </form>
+        <div className="flex mt-5 flex-col lg:flex-row lg:px-7 gap-4 p-4 w-full">
+
+        
+            <Button variant="contained" onClick={UnblockUser} color='success' startIcon={<CheckCircleIcon/>}>Unblock</Button>
+            <Button variant="contained" onClick={blockUser} sx={{backgroundColor : red[300]}} startIcon={<CancelIcon/>} >Block</Button>
+            <Button variant="contained" onClick={delUser} sx={{backgroundColor : red[800]}} startIcon={<DeleteIcon />}>Delete</Button>
+
+            <form className="mr-6 lg:flex-1" onSubmit={(e) => e.preventDefault()}>
+                <label className="font-extrabold mb-5"> Id: </label>
+                {/* <input type="text" name="id" value={TrueId} onChange={(e) => SetId(e.target.value)} className="border-2 w-full" /> */}
+                <TextField id="outlined-basic" label="Input the desired Id..." variant="outlined" value={TrueId} onChange={(e) => SetId(e.target.value)} className='border-2 w-full' />            
+
+                </form>
         </div>
     )
 }
