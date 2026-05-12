1030766d0bd34e14fc4e513a999bc90ddea0a0c3
Leo Girard <leo.girard@epitech.eu>
2026-05-12 12:06:52 +0200

    feat : designing front end components main page

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,30 +1,39 @@
+import { InputBase } from "@mui/material";
+import { Button } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
+
 const Page = () => {
   return (
     <main>
-      <div className="flex flex-col items-start justify-center p-6 sm:p-8 md:p-12">
-        
+      <div className="flex flex-col items-start justify-center p-6 pt-10 sm:p-8 md:p-12">
+
         <div className="flex flex-col items-start gap-4 max-w-xl">
-          
+
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
-            Trouvez le Job <br /> qui VOUS correspond
+            Trouvez rapidement le <span className="text-[#1976d2]">Job</span> <br /> qui  <span className="text-[#1976d2]">VOUS</span> correspond
           </h1>
 
           <h2 className="text-base sm:text-lg md:text-xl text-gray-600">
             {"Des milliers d'offres d'emploi pour booster votre carrière professionnelle"}
           </h2>
 
-          <div className="flex w-full flex-col sm:flex-row items-stretch sm:items-center overflow-hidden rounded-md border">
-            
+          <div className="flex w-full flex-col sm:flex-row items-stretch sm:items-center gap-3 lg:gap-0  ">
+
             <input
               type="text"
-              className="flex-1 px-3 py-2 outline-none"
+              className="flex-1 px-3 py-2 border border-black/50 text-black rounded-md "
               placeholder="Rechercher..."
             />
 
-            <button className="bg-blue-500 px-4 py-2 text-white">
-              Search
+            <button className="bg-[#1976d2] px-4 py-2 sm:ml-3  text-white shadow-md rounded-md">
+              <SearchIcon/> Search
             </button>
+
+
           </div>
+
+
 
         </div>
       </div>
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -17,6 +18,11 @@ export const metadata: Metadata = {
   description: "find your dream job and blow up",
 };
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
