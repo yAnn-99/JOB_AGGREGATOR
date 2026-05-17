fce915fade48b7ebaa0d38c77d9a2bacea5c3ba1
Matteo <thibaudierematteo@gmail.com>
2026-05-18 00:34:01 +0200

    feat: add 5 new Table for register data

--- backend/database/db.sql
+++ backend/database/db.sql
@@ -1,30 +1,76 @@
-
+DROP TABLE IF EXISTS "user_skill";
+DROP TABLE IF EXISTS "skill";
 DROP TABLE IF EXISTS "experience";
 DROP TABLE IF EXISTS "user";
 
+/* USER */
+
 CREATE TABLE "user" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "email" varchar(100) NOT NULL UNIQUE,
-  "password" varchar(100) NOT NULL,
-  "lastname" varchar(100) NOT NULL,
-  "firstname" varchar(100) NOT NULL
+ "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY 
+ "email" VARCHAR(100) NOT NULL UNIQUE 
+ "password" VARCHAR(255) NOT NULL 
+ "lastname" VARCHAR(100) NOT NULL 
+ "firstname" VARCHAR(100) NOT NULL 
+ "created_at" TIMESTAMP DEFAULT NOW()
 );
 
--- need to add a 'suscribed' offer to use in the model'
-
+/* EXPERIENCE */
 
 CREATE TABLE "experience" (
-    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "localization" varchar(100) NOT NULL,
-    "permanent_contract" BOOLEAN NOT NULL, 
-    "skills" INT NOT NULL,
-    "start" varchar(100) NOT NULL,
-    CONSTRAINT chk_start CHECK (
-        "start" = 'asap' OR  
-        "start" ~ '^[0-9]{2}/[0-9]{2}/[0-9]{2}$'
-    ),
-    "user_id" INT NOT NULL , 
-    CONSTRAINT "fk_experience_user"
-        FOREIGN KEY ("user_id")
-        REFERENCES "user" ("id")
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "title" VARCHAR(100) NOT NULL,
+  "localization" VARCHAR(100) NOT NULL,
+  "remote" BOOLEAN DEFAULT false,
+  "permanent_contract" BOOLEAN NOT NULL,
+  "years_experience" INT NOT NULL,
+  "salary_expectation" INT,
+  "start" VARCHAR(100) NOT NULL,
+
+  CONSTRAINT chk_start CHECK (
+    "start" = 'asap' OR "start" ~ '^[0-9]{2}/[0-9]{2}/[0-9]{2}$'
+  ),
+   "user_id" INT NOT NULL,
+  CONSTRAINT "fk_experience_user"
+  FOREIGN KEY ("user_id")
+  REFERENCES "user" ("id")
+);
+
+/* SKILLS */
+
+CREATE TABLE "skill" (
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "name" VARCHAR(100) UNIQUE NOT NULL
+);
+
+/* USER SKILLS */
+
+CREATE TABLE "user_skill" (
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "user_id" INT NOT NULL,
+  "skill_id" INT NOT NULL,
+  "level" INT DEFAULT 1,
+  CONSTRAINT "fk_user_skill_user"
+  FOREIGN KEY ("user_id")
+  REFERENCES "user" ("id"),
+
+  CONSTRAINT "fk_user_skill_skill"
+  FOREIGN KEY ("skill_id")
+  REFERENCES "skill" ("id")
+);
+
+/* FAVORITE JOBS */
+
+CREATE TABLE favorite_job (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id INT NOT NULL,
+  job_id VARCHAR(100) NOT NULL
+);
+
+/* APPLICATIONS (ML)*/
+
+CREATE TABLE application (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id INT NOT NULL,
+  job_id VARCHAR(100) NOT NULL,
+  applied_at TIMESTAMP DEFAULT NOW()
 );
